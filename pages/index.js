87d8f19b2eb09af061bd2432114409930e14762f
Tushar2930 <tusharshingane1234@gmail.com>
2023-07-01 17:20:35 +0530

    matching

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import { useEffect } from 'react';
 import { Inter } from 'next/font/google'
+import {calculateMatchingScore} from "@/utils/calculator"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
