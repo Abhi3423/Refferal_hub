3f45d831683dc65a7e8558a12bdc99a61efafc44
Tushar2930 <tusharshingane1234@gmail.com>
2023-07-02 01:07:56 +0530

    referral approved

--- pages/dashboard/approved-referrals.js
+++ pages/dashboard/approved-referrals.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React ,{useState,useEffect}from 'react'
 import Layout from '@/components/manager_shared/layout'
 
 function approved_referrals() {
+
+  const [user, setUser] = React.useState([])
+  async function getUser() {
+    //post request paasing email
+     const res = await fetch("/api/user/getuser", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify({ email: "tushar@gmail.com" }),
+     });
+     const data = await res.json();
+    const temp=data.data.referal_accepted;
+    //sort temp array in descending order of resume score
+    setUser(temp);
+   }
+   useEffect(() => {
+     getUser();
+   }, []);
+
   return (
     <Layout>
       <div className='w-full bg-white rounded-lg p-4'>
-        approved
+      <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+    <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
+        <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+            <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+              {/* title */}
+            </div>
+            <div className="overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-4 py-3">Sr. No.</th>
+                            <th scope="col" className="px-4 py-3">User name</th>
+                            <th scope="col" className="px-4 py-3">User Email</th>
+                        </tr>
+                    </thead>
+                   
+                    <tbody>
+                    {user.map((item,key)=>{
+                        return <tr className="border-b dark:border-gray-700 ">
+                        <td className="px-4 py-3">{key+1}</td>
+                            <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.name}</th>
+                            <td className="px-4 py-3">{item.userEmail}</td>
+                            <td className="px-4 py-3">{item.resumeScore}</td>
+                        </tr>
+                      })}
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    </div>
+    </section>
       </div>
     </Layout>
   )
--- pages/dashboard/invite-referrals.js
+++ pages/dashboard/invite-referrals.js
@@ -36,6 +36,7 @@ console.log(user)
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
+                        <th scope="col" className="px-4 py-3">Sr. No.</th>
                             <th scope="col" className="px-4 py-3">User name</th>
                             <th scope="col" className="px-4 py-3">User Email</th>
                             <th scope="col" className="px-4 py-3">Resume Score</th>
@@ -44,9 +45,9 @@ console.log(user)
                     </thead>
                    
                     <tbody>
-                    {user.map((item)=>{
+                    {user.map((item,key)=>{
                         return <tr className="border-b dark:border-gray-700 ">
-
+                        <td className="px-4 py-3">{key+1}</td>
                             <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.name}</th>
                             <td className="px-4 py-3">{item.userEmail}</td>
                             <td className="px-4 py-3">{item.resumeScore}</td>
