aa0a4a97c6024caa6f8cc3629d4b8a009620f91e
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-07-01 22:48:16 +0530

    layout created

---
+++ pages/dashboard/invite-referrals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '@/components/manager_shared/layout'
+
+function invite_referrals() {
+  return (
+    <Layout>
+      <div className='w-full bg-white rounded-lg p-4'>
+        invite
+      </div>
+    </Layout>
+  )
+}
+
+export default invite_referrals
---
+++ pages/dashboard/profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '@/components/manager_shared/layout'
+
+function profile() {
+  return (
+    <Layout>
+      <div className='w-full bg-white rounded-lg p-4'>
+        profile
+      </div>
+    </Layout>
+  )
+}
+
+export default profile
