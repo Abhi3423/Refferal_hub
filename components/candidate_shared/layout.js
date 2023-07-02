ccea5f8c66bce653265d51f09d87f0612aa2645c
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-07-02 21:20:56 +0530

    find referral css done

--- components/candidate_shared/layout.js
+++ components/candidate_shared/layout.js
@@ -4,7 +4,7 @@ import Sidebar from './sidebar'
 const Layout = ({ children }) => {
     return (
         <div>
-            <main className="flex w-full h-screen">
+            <main className="flex w-full">
                 <Sidebar>
                     {children}
                 </Sidebar>
