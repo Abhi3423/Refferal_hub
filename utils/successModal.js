6f611be3fefdbf6f9d482329ea5da4f7da2d9a0e
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-07-03 00:04:26 +0530

    Create successModal.js

---
+++ utils/successModal.js
@@ -0,0 +1,23 @@
+import { Fragment, useState } from "react";
+
+const SuccessModal = ({ children, successState }) => {
+  const [showModal, setShowModal] = useState(successState);
+  return (
+    <Fragment>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="p-6 max-h-[90vh] overflow-auto">{children}</div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </Fragment>
+  );
+};
+
+export default SuccessModal;
