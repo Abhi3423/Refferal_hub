47bb720ce48ba3af1dab15d59e78f4865fbe08d6
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-07-02 18:39:53 +0530

    user form

--- components/Forms/Candidate/resume.js
+++ components/Forms/Candidate/resume.js
@@ -12,12 +12,10 @@ const Resume_info = () => {
 
   const formik = useFormik({
     initialValues: {
-      yourField: "",
       resume: "",
     },
     validationSchema: Yup.object().shape({
-      // resume: Yup.string().required("* Resume is required"),
-      // yourRole: Yup.string().required("* Your role is required"),
+      resume: Yup.string().required("* Resume is required"),
     }),
     onSubmit: async (values) => {
       console.log(values);
@@ -33,7 +31,7 @@ const Resume_info = () => {
             name: user?.currentUserDetails?.name,
             photo: user?.currentUserDetails?.photo,
             ...fullDetails,
-            company: values,
+            resume: values,
           }),
         });
         console.log(res);
@@ -63,7 +61,27 @@ const Resume_info = () => {
       <div>
         <div className="flex justify-center items-center w-full">
         </div>
-        <div className="w-full flex justify-center items-center mt-4">
+        <div className="flex justify-center items-center w-full">
+          <div className="font-normal text-sm grid grid-cols-2 gap-x-40 items-center">
+            <label htmlFor="org">Resume Upload Link </label>
+            <input
+              className="w-fit px-4 py-2 border-solid border-[1.5px] border-[#E5E5E5] rounded-md font-semibold"
+              type="text"
+              name="resume"
+              value={formik.values.resume}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.resume && formik.errors.resume ? (
+              <div className="text-red-500 text-xs col-start-2 mt-1">
+                {formik.errors.resume}
+              </div>
+            ) : (
+              <div className="text-red-500 text-xs col-start-2 mt-1 w-full h-4"></div>
+            )}
+          </div>
+        </div>
+        {/* <div className="w-full flex justify-center items-center mt-4">
           <div class="flex items-center justify-center w-1/4 ">
             <label
               for="dropzone-file"
@@ -87,7 +105,7 @@ const Resume_info = () => {
               />
             </label>
           </div>
-        </div>
+        </div> */}
         <div className="flex justify-center items-center w-full">
           <button
             className="bg-green-500 w-fit py-2 px-4 rounded-lg text-white"
---
+++ pages/type/user/profile.js
@@ -0,0 +1,131 @@
+import { React, useEffect,useState } from "react";
+import Layout from "@/components/candidate_shared/layout";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
+import Image from "next/image";
+
+function Profile() {
+  const user = useContext(AuthContext);
+  const [userDetails, setUserDetails] = useState({});
+  const [email, setEmail] = useState("");
+
+  async function getUserDetails() {
+    //post request to backend giving email
+    //get response and set it to userDetails,
+    const response = await fetch("/api/user/getuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
+    });
+    const data = await response.json();
+    // console.log(email);
+    setUserDetails(data.data);
+  }
+
+  useEffect(() => {
+    setEmail(user?.currentUserDetails?.email);
+  }, [user?.currentUserDetails]);
+  useEffect(()=>{
+    if(email){
+    getUserDetails();
+    }
+  },[email])
+;
+  // console.log(userDetails);
+  return (
+    <Layout>
+      <div className="w-full bg-white rounded-lg p-4 h-[85vh]">
+        <div className="flex gap-3 w-full items-center">
+          <span className="block h-[0.5px] w-[4%] bg-slate-400 top-1/2 right-0 transform -translate-y-1/2"></span>
+          <span className="text-sm font-semibold whitespace-nowrap">
+            PERSONAL DETAILS
+          </span>
+          <span className="block h-[0.5px] w-[70%] bg-slate-400 top-1/2 left-0 transform -translate-y-1/2"></span>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="flex justify-center w-full my-2">
+            <Image
+              className="rounded-full"
+              width={90}
+              height={90}
+              src={userDetails?.photo}
+              alt="user"
+            />
+          </div>
+
+          <div className="flex justify-center items-center w-full">
+            <div className="font-bold text-sm grid grid-rows-2 md:grid-cols-2 gap-x-40 items-center md:text-center">
+              <label htmlFor="name">Name </label>
+              <input
+                className="w-fit px-4 py-2 border-solid border-[1.5px] border-[#E5E5E5] rounded-md font-semibold"
+                type="text"
+                name="name"
+                value={userDetails?.name}
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center w-full">
+            <div className="font-bold text-sm grid grid-rows-2 md:grid-cols-2 gap-x-40 items-center md:text-center">
+              <label htmlFor="gmail">Gmail </label>
+              <input
+                className="w-fit px-4 py-2 border-solid border-[1.5px] border-[#E5E5E5] rounded-md font-semibold"
+                type="text"
+                name="gmail"
+                value={userDetails?.email}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center items-center w-full">
+            <div className="font-bold text-sm grid grid-rows-2 md:grid-cols-2 gap-x-40 items-center md:text-center">
+              <label htmlFor="gmail">Phone Number </label>
+              <input
+                className="w-fit px-4 py-2 border-solid border-[1.5px] border-[#E5E5E5] rounded-md font-semibold"
+                type="text"
+                name="gmail"
+                value={userDetails?.personal?.phoneNumber}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center items-center w-full">
+            <div className="font-bold text-sm grid grid-rows-2 md:grid-cols-2 gap-x-40 items-center md:text-center">
+              <label htmlFor="gmail">Address </label>
+              <input
+                className="w-fit px-4 py-2 border-solid border-[1.5px] border-[#E5E5E5] rounded-md font-semibold"
+                type="text"
+                name="gmail"
+                value={userDetails?.personal?.state + ", " + userDetails?.personal?.country} 
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-3 w-full items-center">
+          <span className="block h-[0.5px] w-[4%] bg-slate-400 top-1/2 right-0 transform -translate-y-1/2"></span>
+          <span className="text-sm font-semibold whitespace-nowrap">
+            COMPANY DETAILS
+          </span>
+          <span className="block h-[0.5px] w-[70%] bg-slate-400 top-1/2 left-0 transform -translate-y-1/2">
+          
+          </span>
+          
+        </div>
+        <div className="flex justify-center items-center w-full">
+            <div className="font-bold text-sm grid grid-rows-2 md:grid-cols-2 gap-x-40 items-center md:text-center">
+              <label htmlFor="gmail">Resume</label>
+              <input
+                className="w-fit px-4 py-2 border-solid border-[1.5px] border-[#E5E5E5] rounded-md font-semibold"
+                type="text"
+                name="gmail"
+                value={userDetails?.resume?.resume} 
+              />
+            </div>
+          </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Profile;
