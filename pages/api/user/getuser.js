8595d3ca525631030273df171d5432ed5d139620
just-ctrlC-ctrlV <109028862+just-ctrlC-ctrlV@users.noreply.github.com>
2023-07-02 06:14:25 +0530

    bigfix

--- pages/api/user/getuser.js
+++ pages/api/user/getuser.js
@@ -1,25 +1,27 @@
 import { db } from "../../../database/firestoreConfig";
 import { collection, getDocs, addDoc, where, query } from "firebase/firestore";
 
+export default async function temp(req, res) {
+  try {
+    const { email } = await req.body;
+    console.log(email);
+    //find user with email id
+    const q = query(collection(db, "refferar"), where("email", "==", email));
+    const querySnapshot = await getDocs(q);
 
-export default async function temp(req,res){
-    try {
-       const email=await req.body.email;
-       //find user with email id 
-         const q = query(collection(db, "refferar"), where("email", "==", email));
-            const querySnapshot = await getDocs(q);
-            const data={};
-            querySnapshot.forEach((doc) => {
-                data.id=doc.id;
-                data.name=doc.data().name;
-                data.email=doc.data().email;
-                data.phone=doc.data().phone;
-                data.referal_accepted=doc.data().referal_accepted;
-                data.referal_request=doc.data().referal_request;
-            });
-            res.status(200).json({message:"success",data:data});
+    let data = {};
 
-    } catch (error) {
-        console.log(error);
-    }
+    querySnapshot.forEach((doc) => {
+      data.id = doc.id;
+      data.name = doc.data().name;
+      data.email = doc.data().email;
+      data.phone = doc.data().phone;
+      data.referal_accepted = doc.data().referal_accepted;
+      data.referal_request = doc.data().referal_request;
+    });
+    res.status(200).json({ message: "success", data: data });
+  } catch (error) {
+    res.status(400).json({ message: "error", data: error });
+    console.log(error);
+  }
 }
