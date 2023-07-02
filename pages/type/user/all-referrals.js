005ee378fa92de9c29ec25ec010d2d14e23aeb16
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-07-02 23:03:09 +0530

    Update all-referrals.js

--- pages/type/user/all-referrals.js
+++ pages/type/user/all-referrals.js
@@ -105,7 +105,7 @@ console.log(resumeScore)
                     <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">{item?.refForm?.position}</h2>
                     <h4 className=" pb-4 font-normal">{item?.refForm?.company_name}</h4>
                     <div className="flex pb-2 items-center ">
-                      <svg class="w-4 h-4 mr-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                      <svg class="w-4 h-4 mr-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9" />
                       </svg>{item?.refForm?.city}
                     </div>
